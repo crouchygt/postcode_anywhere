a8e255323fc92a491581e720e4ba0ac5ff7446ae
Matthew Crouch <mcrouch@mobilezil.la>
2011-05-31 16:35:19 +0100

    Working postcode finder

--- lib/postcode_anywhere.rb
+++ lib/postcode_anywhere.rb
@@ -1,29 +1,39 @@
 require "httparty"
-class PostcodeAnywhere
+module PostcodeAnywhere
+  
+  SERVICE_ADDRESS = "http://services.postcodeanywhere.co.uk/PostcodeAnywhere/Interactive/RetrieveByPostcodeAndBuilding/v1.00/xmle.ws"
   
   include HTTParty
   format :xml
-
-  SERVICE_ADDRESS = "http://services.postcodeanywhere.co.uk/PostcodeAnywhere/Interactive/RetrieveByPostcodeAndBuilding/v1.00/xmle.ws"
-  POSTCODE_ANYWHERE_KEY = 0
+  
+  class << self
     
+    attr_accessor :key
+    
+    def license_key
+      @key
+    end
+  end
+  
   def self.find_by_number_and_postcode(number, postcode)
-    validate_key
+    PostcodeAnywhere.validate_key
     sanitised_postcode = postcode.gsub(/\s/, "")
-    data = lookup(number, sanitised_postcode)
+    data = PostcodeAnywhere.lookup(number, sanitised_postcode)
     data["Table"]["Row"]
   end
   
   protected
   
-  def validate_key
-    if POSTCODE_ANYWHERE_KEY == 0
+  def self.validate_key
+    unless PostcodeAnywhere.key
       raise PostcodeAnywhereException, "Please provide a valid Postcode Anywhere License Key"
     end
   end
 
-  def lookup(number, postcode)
-    PostcodeAnywhere.get SERVICE_ADDRESS+"?Key=#{POSTCODE_ANYWHERE_KEY}&Postcode=#{postcode}&Building=#{number}"
+  def self.lookup(number, postcode)
+    PostcodeAnywhere.get SERVICE_ADDRESS+"?Key=#{PostcodeAnywhere.key}&Postcode=#{postcode}&Building=#{number}"
   end
+  
+  class PostcodeAnywhereException < StandardError;end
+  
 end
-class PostcodeAnywhereException < StandardError;end
